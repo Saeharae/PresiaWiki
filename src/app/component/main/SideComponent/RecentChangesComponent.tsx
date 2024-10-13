ad0efec7e461ee05ee89fe35197b295932b1e890
SPNIA☆ <53849587+spnia@users.noreply.github.com>
2024-10-14 00:24:17 +0900

    base

--- src/app/component/main/SideComponent/RecentChangesComponent.tsx
+++ src/app/component/main/SideComponent/RecentChangesComponent.tsx
@@ -4,16 +4,16 @@ import styled from 'styled-components';
 export const MainBase = styled.div`
     background-color: #fff;
     border-radius: 10px;
-    width: 1029px;
-    height: 700px;
+    width: 375px;
+    height: 325px;
     margin-top: 100px;
 `;
 
-function Docu_Compo() {
+function ReCh_Compo() {
     return (
         <MainBase>
         </MainBase>
     );
 }
 
-export default Docu_Compo;
+export default ReCh_Compo;
--- src/app/component/main/main/main.tsx
+++ src/app/component/main/main/main.tsx
@@ -1,9 +1,22 @@
+"use client";
 import Docu_Compo from "../DocumentComponent/component";
+import ReCh_Compo from "../SideComponent/RecentChangesComponent";
+import styled from "styled-components";
 
-export default function Main() {
+export const MainBase = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: -100px;
+    gap: 30px;
+`;
+
+function Main() {
   return (
-    <div>
+    <MainBase>
       <Docu_Compo />
-    </div>
+      <ReCh_Compo />
+    </MainBase>
   );
 }
+
+export default Main;
